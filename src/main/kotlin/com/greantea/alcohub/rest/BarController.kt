9fa4caf205ba2e1103669a2a3ac0a0211a4ec377
Arthur Aukhatov <aaukhatov@gmail.com>
2018-06-02 22:44:37 +0300

    simple mock rest api for the Bar entity

--- src/main/kotlin/com/greantea/alcohub/rest/BarController.kt
+++ src/main/kotlin/com/greantea/alcohub/rest/BarController.kt
@@ -1,13 +1,30 @@
 package com.greantea.alcohub.rest
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.greantea.alcohub.domain.Bar
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/bars")
 class BarController {
 
+    @GetMapping("/{id}")
+    fun getBar(@PathVariable("id") id: String): Bar {
+        return Bar("Bar 108")
+    }
+
     @GetMapping
-    fun getBar(): String {
-        return "Response"
+    fun getBarList(): List<Bar> {
+        return listOf(Bar("Bar 108"), Bar("Another Bar"))
+    }
+
+    @PutMapping("/{id}")
+    fun updateBar(@PathVariable("id") id: String,
+                  @RequestBody bar: Bar): Bar {
+        return bar
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteBar(@PathVariable("id") id: String) {
+
     }
 }
